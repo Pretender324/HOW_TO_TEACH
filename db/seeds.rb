a7728f05aba35291aef06eb0de768f00c6bd4891
安達陸 <adachiriku@adachiriku-no-MacBook-Air.local>
2020-04-21 21:45:17 +0900

    modified_3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(:email => 'test@test.com', :password => 'hugahuga')
+user.save!
+
+5.times do |n|
+    Post.create!(
+        title: "test#{n + 1}",
+        contents: "テスト太郎#{n + 1}",
+        style: "集団"
+        subject: "数学"
+        grade: "中学生"
+        user_id: "1"
+    )
+  end
